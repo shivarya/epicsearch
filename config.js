f05325f495c4fe6e71f789a97ad1b3641125960b
Master Silver <ayush.ghai@mindgrep.com>
2015-09-01 04:29:21 +0530

    removed space between 'key :' to make it key:

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports =   {
     clientParams:{
-      hosts : [{'host': 'localhost', 'protocol': 'http', 'port': 9200}],
+      hosts: [{'host': 'localhost', 'protocol': 'http', 'port': 9200}],
       requestTimeout: 90000,
       maxConnections: 200
     },
     /**cloneClientParams:{
-      hosts : [{'host': 'ep-st1', 'protocol': 'http', 'port': 9200}],
+      hosts: [{'host': 'ep-st1', 'protocol': 'http', 'port': 9200}],
       requestTimeout: 90000,
       maxConnections: 200
     },**/
-    default_index : 'test',
+    default_index: 'test',
     default_type: 'test',
     percolate:{
       query_index: 'queries'
--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var
     mget: './lib/get/mget',
     search: './lib/search/index',
     msearch: './lib/search/multi_search',
-    mpu : './lib/percolate/mpu',
-    get_first : './lib/get/first',
-    get_dups : './lib/get/dups',
-    delete_dups : './lib/delete/delete_dups',
-    find_and_delete_dups : './lib/delete/find_delete_dups',
+    mpu: './lib/percolate/mpu',
+    get_first: './lib/get/first',
+    get_dups: './lib/get/dups',
+    delete_dups: './lib/delete/delete_dups',
+    find_and_delete_dups: './lib/delete/find_delete_dups',
     index_by_unique: './lib/index/byUniqueKey',
-    bulk_index : './lib/index/bulk',
-    index : './lib/index/index'
+    bulk_index: './lib/index/bulk',
+    index: './lib/index/index'
   }
 
 var EpicSearch = function(config){
